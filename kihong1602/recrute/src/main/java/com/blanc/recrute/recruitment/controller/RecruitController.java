73f88a2aa11b31b9628c031a346adf44ff03b35d
Kihong <kks4517@naver.com>
2023-10-19 17:00:02 +0900

    Feat: 채용지원 기능 구현
    - apply.js: 비동기 통신으로 채용지원 요청
    - RecruitController: JSON 파싱과 AuthCookie 조회 후 채용공고 지원, 응답결과 JSON타입으로 전송하는 기능 구현
    - ApplyInfoDTO: JSON key value 매핑을 위한 DTO 구현
    - ApplyService: aptId 생성을 위해 memberId로 연계된 member table id 조회, aptId 생성 기능 구현, DAO 반환 값 검증
    - ApplyDAO: DB에 응시지원 데이터를 저장하는 기능 구현
    - CookieManager: 쿠키 조회기능 구현
    - AptIdFactory: aptId 생성기능 구현
    
    Issue: #76

--- kihong1602/recrute/src/main/java/com/blanc/recrute/recruitment/controller/RecruitController.java
+++ kihong1602/recrute/src/main/java/com/blanc/recrute/recruitment/controller/RecruitController.java
@@ -1,11 +1,17 @@
 package com.blanc.recrute.recruitment.controller;
 
+import com.blanc.recrute.common.CookieManager;
+import com.blanc.recrute.common.JsonUtil;
 import com.blanc.recrute.common.ViewResolver;
+import com.blanc.recrute.member.dto.InvalidDTO;
+import com.blanc.recrute.recruitment.dto.ApplyInfoDTO;
 import com.blanc.recrute.recruitment.dto.DetailDTO;
 import com.blanc.recrute.recruitment.service.ApplyService;
 import com.blanc.recrute.recruitment.service.RecruitService;
+import com.google.gson.Gson;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -38,5 +44,25 @@ public class RecruitController extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //비동기 처리 apt_id 는 회사ID+회사Name+응시순서+@로 입력
         //외래키 삽입을 위해 MemberDTO 사용해야함
+        
+        String parsingJSON = JsonUtil.jsonParsing(request);
+
+        ApplyInfoDTO applyInfoDTO = new Gson().fromJson(parsingJSON, ApplyInfoDTO.class);
+
+        Cookie AuthCookie = CookieManager.getCookie(request, "sid");
+
+        if (AuthCookie != null) {
+
+            String memberId = (String) request.getSession().getAttribute(AuthCookie.getValue());
+
+            String result = applyService.applyRecruit(applyInfoDTO, memberId);
+
+            InvalidDTO invalidDTO = result.equals("success") ? new InvalidDTO("available") : new InvalidDTO("unavailable");
+
+            String json = new Gson().toJson(invalidDTO);
+
+            JsonUtil.sendJSON(response, json);
+
+        }//end of if
     }
 }
