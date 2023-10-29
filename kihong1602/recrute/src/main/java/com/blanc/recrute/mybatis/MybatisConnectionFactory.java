37f8e05125f0b4a3211c4778a15d5390b7736693
Kihong <kks4517@naver.com>
2023-10-30 08:16:42 +0900

    Chore: DB연결 스레드 연결방식 변경
    - ThreadLocal 사용으로 스레드간 독립적인 SqlSession을 유지하게 변경

--- kihong1602/recrute/src/main/java/com/blanc/recrute/mybatis/MybatisConnectionFactory.java
+++ kihong1602/recrute/src/main/java/com/blanc/recrute/mybatis/MybatisConnectionFactory.java
@@ -1,31 +1,53 @@
 package com.blanc.recrute.mybatis;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 public class MybatisConnectionFactory {
 
-    private static SqlSessionFactory sqlSessionFactory;
-    private static final Logger logger = Logger.getLogger(MybatisConnectionFactory.class.getName());
+  private static final Logger logger = Logger.getLogger(MybatisConnectionFactory.class.getName());
 
-    static {
-        try {
-            String resource = "config/mybatis-config.xml";
-            InputStream inputStream = Resources.getResourceAsStream(resource);
-            sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-        } catch (IOException e) {
-            logger.log(Level.SEVERE, "Error!!", e);
-        }
-    }
+  private static ThreadLocal<SqlSession> sqlSessionThreadLocal = ThreadLocal.withInitial(
+      () -> null);
 
-    public static SqlSession getSqlSession() {
-        return sqlSessionFactory.openSession(true);
+  private MybatisConnectionFactory() {
+  }
+
+  private static SqlSessionFactory initializeSqlSessionFactory() {
+
+    try {
+      String resource = "config/mybatis-config.xml";
+      InputStream inputStream = Resources.getResourceAsStream(resource);
+      return new SqlSessionFactoryBuilder().build(inputStream);
+    } catch (IOException e) {
+      logger.log(Level.SEVERE, "Error!", e);
+      throw new RuntimeException("Failed to initialize MyBatis", e);
     }
+  }
+
+  public static SqlSession getSqlSession() {
+    SqlSession sqlSession = sqlSessionThreadLocal.get();
+    if (sqlSession == null) {
+      sqlSession = initializeSqlSessionFactory().openSession();
+      sqlSessionThreadLocal.set(sqlSession);
+    }
+    return sqlSession;
+  }
+
+
+  public static void closeSqlSession() {
+    SqlSession sqlSession = sqlSessionThreadLocal.get();
+    if (sqlSession != null) {
+      sqlSessionThreadLocal.remove();
+      sqlSession.close();
+    }
+  }
+
+
 }
